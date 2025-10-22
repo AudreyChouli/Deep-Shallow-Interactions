0c1b9a16df128b0360a1b5372ca10b28f636f2bb
AudreyChouli <audreychouli@aol.fr>
2025-10-22 16:22:51 +0200

    Clean up test_functions_2025.py
    Removed unused code and comments to clean up the script.

--- test_functions_2025.py
+++ test_functions_2025.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,8 +10,6 @@ from datetime import datetime
 from datetime import timedelta
 from scipy.interpolate import griddata
 from math import *
-
-
 
 
 def isodepth_function(m):
@@ -72,19 +65,7 @@ def isodepth_function(m):
     plt.plot(map_lon_trench, map_lat_trench, marker = 8, color='black', zorder = 2)
 
 
-    """
-    # Slab
-    map_lat_slab = []
-    map_lon_slab = []
-    for i in range(len(slab_japan_up)):
-        x, y = m(slab_japan_up['Lon'][i], slab_japan_up['Lat'][i])
-        map_lon_slab.append(x)
-        map_lat_slab.append(y)
-    m.scatter(map_lon_slab, map_lat_slab, c = slab_japan_up["Depth"], cmap=plt.cm.gist_earth, alpha = 0.2, zorder = 2)
-    """
-    
-    
-    
+
 """   
 def add_slab_depth(catalog_japan):
     #### Slab surface Hayes 2018
@@ -115,23 +96,12 @@ def find_tohoku(catalog_japan):
     #print("'tohoku' :")
     #display(tohoku)
     #return(tohoku)
-    
-    
+     
     
     
 def selection_catalog(catalog_japan, t_min, t_max , mc, limit_depth_deep, limit_depth_shallow, lat_min, lat_max, lon_min, lon_min_shallow, lon_max):
     # New column in catalog japan containing the distance from Tohoku epicenter
     # catalog_japan["Dist_Tohoku"] = np.sqrt(((catalog_japan.Lat.values - tohoku.Lat.values)*111)**2 + ((catalog_japan.Lon.values - tohoku.Lon.values)*111*np.cos(np.radians(tohoku.Lat.values)))**2)
-
-    
-
-    # Define the interest area
-    #lat_min         = 37  
-    #lat_max         = 40   
-    #lon_min         = 140
-    #lon_min_shallow = lon_min # to change area selected for shallow EQs
-    #lon_max         = 144
-
 
     # Conditions
     c_latmin          = (catalog_japan.Lat.values>lat_min)
@@ -143,7 +113,7 @@ def selection_catalog(catalog_japan, t_min, t_max , mc, limit_depth_deep, limit_
     c_tmax            = (catalog_japan["Date_UTC"]<(t_max))
     c_tmin            = (catalog_japan["Date_UTC"]>(t_min))
     c_mag             = (catalog_japan.Mag >= mc)
-    c_deep            = ((catalog_japan.Depth >= limit_depth_deep) & (catalog_japan['Distance to top slab'] <= 0)) #, under slab surface 
+    c_deep            = ((catalog_japan.Depth >= limit_depth_deep) & (catalog_japan['Distance to top slab'] <= 0))
     c_shallow         = ((catalog_japan.Depth < limit_depth_shallow) & (catalog_japan['Distance to top slab']<= 5))
     c_between         = ((catalog_japan.Depth < limit_depth_deep) & (catalog_japan.Depth >= limit_depth_shallow)  & (catalog_japan['Distance to top slab']<= 0))
 
@@ -152,7 +122,7 @@ def selection_catalog(catalog_japan, t_min, t_max , mc, limit_depth_deep, limit_
     EQ_shallow   = catalog_japan[c_latmin & c_latmax & c_lonmin_shallow & c_lonmax & c_tmax & c_tmin & c_mag & c_shallow]
     EQ_between   = catalog_japan[c_latmin & c_latmax & c_lonmin_shallow & c_lonmax & c_tmax & c_tmin & c_mag & c_between]
 
-    # Zone plus large pour compter les M>=6
+    # Define larger area to count M>=6 earthquakes
     large_extent = 4 #degres to add to extend the area
     c_latmin_large          = (catalog_japan.Lat.values>lat_min-large_extent)
     c_latmax_large          = (catalog_japan.Lat.values<lat_max+large_extent)
@@ -178,8 +148,6 @@ def count_events(catalog, start, end, m_min):
         return( len(catalog[(catalog['Date_UTC'] >= start) & (catalog['Date_UTC'] <= end)]))
     else :
         return( len(catalog[(catalog['Date_UTC'] >= start) & (catalog['Date_UTC'] <= end) & (catalog['Mag'] >= m_min)]) )
-
-
 
 
 
@@ -211,8 +179,6 @@ def add_slab_depth_kita(catalog_japan):
     slab_kita_interpolated["Lon"]   = slab_japan_up["Lon"].values
     slab_kita_interpolated["Lat"]   = slab_japan_up["Lat"].values
 
-
-
     # Distance to top slab of earthquakes
     # Interpolation to have slab depth at each EQ position (lon-lat)
     lon_to_find_cat                 = catalog_japan["Lon"]
@@ -227,25 +193,8 @@ def add_slab_depth_kita(catalog_japan):
     catalog_japan["Distance to top slab"] = catalog_japan["Slab Depth"] - catalog_japan["Depth"]
     return(catalog_japan)
 
-# In[2]:
 
-
-""" 
-EQ_shallow_large[" Flag "] = 1
-EQ_deep_large[" Flag "]    = 2
-EQ_selection               = pd.concat((EQ_shallow_large, EQ_deep_large)).sort_values(by = "Date UTC")
-EQ_selection_2             = pd.DataFrame(columns=['Date UTC', 'Flag'])
-EQ_selection_2["Date UTC"] = EQ_selection["Date UTC"]
-EQ_selection_2["Flag"]     = EQ_selection[" Flag "]
-EQ_selection_2
-EQ_selection_2.to_csv("/Users/choulia/Documents/Doctorat/catalogues/Japan/EQ_selection_2.csv", index=None)
-"""
-
-
-# In[ ]:
-
-
-#Projection orthogonale pour prendre section perpendiculaire à la trench
+# Orthogonal projection to take a cross-section perpendicular to the trench
 def proj_ortho(lon_start_point,lat_start_point,slope_angle,all_lon_events,all_lat_events):
     from pyproj import Proj
 
@@ -274,8 +223,6 @@ def proj_ortho(lon_start_point,lat_start_point,slope_angle,all_lon_events,all_la
         all_dist[i] = dist
         all_x_proj_section[i] = x_proj_section
     return(all_dist, all_x_proj_section)
-
-
 
 
 def map_background(lon_min, lon_max, lat_min, lat_max):
@@ -310,6 +257,7 @@ def plot_trench(m, lon_min, lon_max, lat_min, lat_max):
     #plt.plot(map_lon_trench, map_lat_trench, color='black', zorder = 2)
     plt.plot(map_lon_trench, map_lat_trench, marker = 8, color='black', zorder = 2, markersize=9 )
 
+    
 def interpolate_slab_kita(lon_min, lon_max, lat_min, lat_max):
     # Read Slab 2
     slab_japan_up = pd.read_csv("/Users/choulia/Documents/DOCTORAT/CODES/Autres/For_map/Slab/Japan_slab2_dep_02.24.18.xyz", names = ["Lon", "Lat", "Depth"])
